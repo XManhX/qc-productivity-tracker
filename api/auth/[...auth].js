5604b8ab6944449ffbe0a139f29c070de6c2e1ad
XManhX <xuanmanh.nguyen@shopee.com>
2026-07-28 16:13:49 +0700

    update

--- api/auth/[...auth].js
+++ api/auth/[...auth].js
@@ -20,6 +20,7 @@ export default async function handler(req, res) {
         if (req.method === 'POST') {
             return loginHandler(req, res);
         }
+        res.setHeader('Allow', ['POST']);
         return res.status(405).json({ error: 'Method not allowed' });
     }
 

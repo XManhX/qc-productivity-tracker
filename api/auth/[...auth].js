a8c91214e7182197e403f48a476b77f66404a5e0
XManhX <xuanmanh.nguyen@shopee.com>
2026-07-28 14:51:47 +0700

    new update auth

--- api/auth/[...auth].js
+++ api/auth/[...auth].js
@@ -1,4 +1,15 @@
 // api/auth/[...auth].js
-export default function handler(req, res) {
-    res.status(200).json({ message: 'Hello from auth', url: req.url });
+import callbackHandler from '../../lib/auth/callback.js';
+import { getPathSegments } from '../../lib/path-utils.js';
+
+export default async function handler(req, res) {
+    console.log('Auth route hit', req.url);
+    const auth = getPathSegments(req, 'auth');
+    // auth = ['seatalk', 'callback'] khi URL là /api/auth/seatalk/callback
+
+    if (auth[0] === 'seatalk' && auth[1] === 'callback' && req.method === 'GET') {
+        return callbackHandler(req, res);
+    }
+
+    return res.status(404).json({ error: 'Auth endpoint not found' });
 }

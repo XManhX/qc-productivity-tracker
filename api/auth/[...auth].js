493171e33da1d238327d68cb4a10f2094a6b134b
XManhX <xuanmanh.nguyen@shopee.com>
2026-07-28 14:37:27 +0700

    test

--- api/auth/[...auth].js
+++ api/auth/[...auth].js
@@ -1,9 +1,4 @@
 // api/auth/[...auth].js
 export default function handler(req, res) {
-    console.log('Auth route hit', req.url);
-    res.status(200).json({
-        message: 'Auth route is working',
-        url: req.url,
-        query: req.query,
-    });
+    res.status(200).json({ message: 'Hello from auth', url: req.url });
 }
